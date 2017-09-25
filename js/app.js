63a127fc88297460ede54a35692d042d5dcc428f
ImYoda <32274335+ImYoda@users.noreply.github.com>
2017-09-25 10:21:43 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+ReactDOM.render(
+			<h1>Hello, world!</h1>,
+			document.getElementById('example')
+			);
 
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
